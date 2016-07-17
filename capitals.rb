91c71e4774f88254c9e1df7a0bc993c1e7474c6d
Nayana Davis <nayana487@gmail.com>
2016-07-17 08:00:29 -0400

    first commit

---
+++ capitals.rb
@@ -0,0 +1,192 @@
+# an array of state hashes
+states =[{
+    name: "Alabama",
+    capital: "Montgomery"
+}, {
+    name: "Alaska",
+    capital: "Juneau"
+}, {
+    name: "Arizona",
+    capital: "Phoenix"
+}, {
+    name: "Arkansas",
+    capital: "Little Rock"
+}, {
+    name: "California",
+    capital: "Sacramento"
+}, {
+    name: "Colorado",
+    capital: "Denver"
+}, {
+    name: "Connecticut",
+    capital: "Hartford"
+}, {
+    name: "Delaware",
+    capital: "Dover"
+}, {
+    name: "Florida",
+    capital: "Tallahassee"
+}, {
+    name: "Georgia",
+    capital: "Atlanta"
+}, {
+    name: "Hawaii",
+    capital: "Honolulu"
+}, {
+    name: "Idaho",
+    capital: "Boise"
+}, {
+    name: "Illinois",
+    capital: "Springfield"
+}, {
+    name: "Indiana",
+    capital: "Indianapolis"
+}, {
+    name: "Iowa",
+    capital: "Des Moines"
+}, {
+    name: "Kansas",
+    capital: "Topeka"
+}, {
+    name: "Kentucky",
+    capital: "Frankfort"
+}, {
+    name: "Louisiana",
+    capital: "Baton Rouge"
+}, {
+    name: "Maine",
+    capital: "Augusta"
+}, {
+    name: "Maryland",
+    capital: "Annapolis"
+}, {
+    name: "Massachusetts",
+    capital: "Boston"
+}, {
+    name: "Michigan",
+    capital: "Lansing"
+}, {
+    name: "Minnesota",
+    capital: "St. Paul"
+}, {
+    name: "Mississippi",
+    capital: "Jackson"
+}, {
+    name: "Missouri",
+    capital: "Jefferson City"
+}, {
+    name: "Montana",
+    capital: "Helena"
+}, {
+    name: "Nebraska",
+    capital: "Lincoln"
+}, {
+    name: "Nevada",
+    capital: "Carson City"
+}, {
+    name: "New Hampshire",
+    capital: "Concord"
+}, {
+    name: "New Jersey",
+    capital: "Trenton"
+}, {
+    name: "New Mexico",
+    capital: "Santa Fe"
+}, {
+    name: "New York",
+    capital: "Albany"
+}, {
+    name: "North Carolina",
+    capital: "Raleigh"
+}, {
+    name: "North Dakota",
+    capital: "Bismarck"
+}, {
+    name: "Ohio",
+    capital: "Columbus"
+}, {
+    name: "Oklahoma",
+    capital: "Oklahoma City"
+}, {
+    name: "Oregon",
+    capital: "Salem"
+}, {
+    name: "Pennsylvania",
+    capital: "Harrisburg"
+}, {
+    name: "Rhode Island",
+    capital: "Providence"
+}, {
+    name: "South Carolina",
+    capital: "Columbia"
+}, {
+    name: "South Dakota",
+    capital: "Pierre"
+}, {
+    name: "Tennessee",
+    capital: "Nashville"
+}, {
+    name: "Texas",
+    capital: "Austin"
+}, {
+    name: "Utah",
+    capital: "Salt Lake City"
+}, {
+    name: "Vermont",
+    capital: "Montpelier"
+}, {
+    name: "Virginia",
+    capital: "Richmond"
+}, {
+    name: "Washington",
+    capital: "Olympia"
+}, {
+    name: "West Virginia",
+    capital: "Charleston"
+}, {
+    name: "Wisconsin",
+    capital: "Madison"
+}, {
+    name: "Wyoming",
+    capital: "Cheyenne"
+}]
+
+#Start with a shuffle to get a random order
+states = states.shuffle
+
+puts "Let's study up on state capitals."
+
+#Initialize new keys to store the number of times a user gets a state right
+# and wrong
+states.each do |state|
+  state[:num_correct] = 0
+  state[:num_wrong] = 0
+end
+
+# until the user decides to end the game...
+while true
+  #Ask the user to name the capital of each state.
+  states.each_with_index do |state, index|
+    puts "What is the capital of " + state[:name] + "?"
+
+    answer = gets.chomp
+    #If the answer is correct, increment the correct counter
+    if answer.upcase == state[:capital].upcase
+      puts "Correct!"
+      state[:num_correct] += 1
+    else
+      #If the answer is wrong, increment the wrong counter
+      puts "Incorrect, the capital is #{state[:capital]}."
+      state[:num_wrong] += 1
+    end
+    # Let the user know success rate
+    total = state[:num_correct] + state[:num_wrong]
+    puts "You are #{state[:num_correct]} for #{total} on that."
+  end
+  # Give the user the option to stop or go through the states again
+  puts "Continue (Y/N)?"
+  answer = gets.chomp
+  break if answer.upcase == "N"
+  # Sort so that the states are in order of fewest correct
+  states = states.sort_by { |state| state[:num_correct]}
+end
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Let's Learn our State Capitals
+
+We're going to create a game to help us memorize the names of the capitals of all 50 states. Your program should prompt the user to identify the capital associated with a given state. There should be running tallies on the number of correct and incorrect answers. After getting through all 50 states one time, users should be asked whether or not they want to play again. As they continue, the prompts on states should appear in the order of the ones the user got wrong the most to least. You're given an array of hashes that contain each state name and capital. *(Hint: For the purposes of developing this program, start with a test array of three hashes so you don't have to play through all 50 states each time.)*
+
+## Methods to Research
+- `.each_with_index`
+- `.sort_by`
+- `.shuffle`
+
+## Use Ruby to implement the following tasks
+
+- Make sure the states don't appear in alphabetical order in the prompts. This will make the game a bit more challenging for the user.
+
+- Provide a welcome message to introduce the play to the game.
+
+- Initialize new keys in the hashes that store the number of times a user gets a capital correct and the number of times the answer is wrong.
+
+- Through all 50 states, prompt the user to name the capital of the state. If the answer is correct, display a message saying so increment the `correct` key. If the answer is wrong, display a message saying so increment the `wrong` key. After each prompt, display a message telling the reader how many times the state was answered correctly out of the total number of times answered.
+
+- Once the user has gone through all 50 states, ask them if they'd like to play again. If they choose to do so, set the order of how the prompts appear to start with the ones they got wrong the most often.
